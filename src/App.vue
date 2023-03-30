bf399f5a7da2c8465476f8a043499becb073fa59
whjin <1414848321@qq.com>
2023-03-30 19:31:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <template>
 	<div class="app">
-		<counter />
+		<Home />
 	</div>
 </template>
 
 <script setup>
-import Counter from './components/Counter.vue'
+import Home from './components/Home.vue'
 </script>
 
 <style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-	transition: filter 300ms;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="home-container">
+		<div class="home-wrapper">
+			<div class="item-box counter">
+				<div>{{num}}</div>
+				<button @click="add">Counter</button>
+			</div>
+			<div class="item-box">
+				<ul>
+					<li v-for="item in studentList" :key="item.id">
+						{{item.id}}--{{item.name}}
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+import studentList from "../static/mock/studentList.json";
+
+let num = ref(0);
+const stuList = reactive(studentList);
+let msg = ref("");
+
+const add = () => {
+	num.value++;
+};
+
+const addStu = () => {
+	const item = { id: stuList.length + 1, name: msg.value };
+	stuList.push(item);
+	
+}
+</script>
+
+<style lang="less">
+</style>
