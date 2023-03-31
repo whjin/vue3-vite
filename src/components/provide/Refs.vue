aa42656ab544fe8622692940af6dccf216b54e8f
whjin <1414848321@qq.com>
2023-03-31 10:54:30 +0800

    update

---
+++ src/components/provide/Refs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="refs-wrapper">
+    <p>{{ msg }}</p>
+    <div>
+      <span>监听值：{{ num }}</span>
+      <span>响应值：{{ watchNum }}</span>
+    </div>
+    <button @click="watchEffectBtn">watchEffect数据监听</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, watchEffect } from "vue";
+
+let msg = ref("Hello Vue3 Ref");
+
+const btn = () => {
+  setTimeout(() => {
+    msg.value = "Hello Vue3 Component Ref";
+  }, 3000);
+};
+
+defineExpose({
+  msg, btn
+});
+
+let num = ref(0);
+let watchNum = ref(0);
+const watchEffectBtn = () => {
+  num.value++;
+};
+watchEffect(() => {
+  watchNum.value = num.value + 2;
+})
+</script>
+
+<style></style>
