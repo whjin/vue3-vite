37cded90fae44b9af7373e86761ac9b04bfcb9a3
whjin <1414848321@qq.com>
2023-09-25 11:52:09 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-	<div class="app">
-		<Home />
-	</div>
+  <div class="app">
+    <Home />
+  </div>
 </template>
 
 <script setup>
-import Home from './components/Home.vue'
+import Home from "./components/Home.vue";
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
