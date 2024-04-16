62d59eb43d84ab13ffff603831345c1b9d8a31d7
whjin <wuhuajin09@163.com>
2024-04-16 23:22:21 +0800

    feat: 调整打包静态文件路径

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
   ],
@@ -13,4 +14,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
