c4c878ed56bea284fb417850e2a214c820daccd6
whjin <wuhuajin09@163.com>
2024-04-27 00:21:58 +0800

    feat: 配置动态base

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,18 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  // base: '/vue3-vite/',
-  base: './',
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  let base = mode == 'production' ? '/vue3-vite/' : './';
+
+  return {
+    base,
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
+  };
 });
