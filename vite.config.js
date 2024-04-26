e3baf0678aae9eea21dc7c5a42a8449866ae0226
whjin <wuhuajin09@163.com>
2024-04-26 23:44:18 +0800

    feat:测试打包vite:base配置

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/vue3-vite/',
+  // base: '/vue3-vite/',
+  base: './',
   plugins: [
     vue(),
   ],
