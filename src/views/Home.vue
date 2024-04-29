ad3ff595dad7003d25bfb05177aeefa46de93c04
whjin <wuhuajin09@163.com>
2024-04-30 00:15:34 +0800

    提交更新main分支

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-container">
+    <main>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/test">Test</RouterLink>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+import { RouterLink } from "vue-router";
+
+</script>
+
+<style lang="less"></style>
