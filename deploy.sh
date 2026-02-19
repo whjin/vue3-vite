879e48559abd73c0737aae246479364a93c3d1e1
whjin <wuhuajin09@163.com>
2026-02-19 14:38:19 +0800

    提交更新main分支

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,12 @@
 # 构建
 npm run build
 
-cd dist
-
-cp index.html 404.html
-
 # 提交main主分支
-git add -A
+git add .
 git commit -m "提交更新main分支"
 git push origin main
+
+cd dist
 
 git push -f git@github.com:whjin/vue3-vite.git main:gh-pages
 
