a624023ae79f02424cad5396753bc12503a195c1
whjin <wuhuajin09@163.com>
2026-02-19 14:45:01 +0800

    feat: 更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,10 @@ npm run build
 # 提交main主分支
 git add .
 git commit -m "提交更新main分支"
-git push origin main
+git push
 
-cd dist
-
-git push -f git@github.com:whjin/vue3-vite.git main:gh-pages
-
-cd -
+# 部署
+npm run deploy
 
 # 按任意键关闭
 echo "按任意键关闭"
